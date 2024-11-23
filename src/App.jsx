b6854991f222089243dd9d006e493eaedfbc6b6d
Furkan Demir <demir@innoscripta.com>
2024-11-23 20:09:10 +0300

    feat: jotai for storing highlighted cells

--- src/App.jsx
+++ src/App.jsx
@@ -49,13 +49,16 @@ function App() {
             </tr>
             </thead>
             <tbody>
-            {filteredData.map((att,i) => {
+            {filteredData.map((att, i) => {
                 console.log(selectedHeights.length % (i + 1))
                 const isLastOfType = (i + 1) % selectedHeights.length === 0
                 return (<tr className={isLastOfType ? 'last' : undefined} key={`${att.Height}-${att.Attribute}`}
-                >{Object.entries(att)
-                    .toSorted((a, b) => order.indexOf(a[0]) - order.indexOf(b[0]))
-                    .map((val) => <Cell key={`${val[0]}`}>{val[1]}</Cell>)}</tr>)
+                >
+                    {Object.entries(att)
+                        .toSorted((a, b) => order.indexOf(a[0]) - order.indexOf(b[0]))
+                        .map((val) => <Cell key={val[0]}
+                                            val={`${att.Height}-${att.Attribute}-${val[0]}`}>{val[1]}</Cell>)}
+                </tr>)
             })}
             </tbody>
         </table>
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,5 +1,13 @@
-import {useState} from "react";
+import {selectedCellsAtom} from "../atoms.js";
+import {useAtom} from "jotai";
+import {memo, useMemo} from "react";
 
+const toggleCell = (selectedCells, val) => {
+    const existingCell = selectedCells.find(cell => cell.val === val)
+    if (existingCell) {
+        return selectedCells.map(cell => ({...cell, color: cell.val === val ? toggleColor(cell.color) : cell.color}))
+    } else return [...selectedCells, {val, color: toggleColor()}]
+}
 
 const toggleColor = (color) => {
     switch (color) {
@@ -11,9 +19,15 @@ const toggleColor = (color) => {
             return undefined
     }
 }
-export const Cell = ({children, ...props}) => {
-    const [highlighted, setHighlighted] = useState();
-    return <td {...props}
-               style={highlighted ? {background: highlighted, color: 'white', fontWeight: 'bold'} : undefined}
-               onClick={() => setHighlighted(prev => toggleColor(prev))}>{children}</td>
-}
+export const Cell = memo(({children, val, ...props}) => {
+        const [selectedCells, setSelectedCells] = useAtom(selectedCellsAtom);
+        const highlightVal = useMemo(() => selectedCells.find(cell => cell.val === val), [selectedCells, val])
+        return <td {...props}
+                   style={highlightVal?.color ? {
+                       background: highlightVal.color,
+                       color: 'white',
+                       fontWeight: 'bold'
+                   } : undefined}
+                   onClick={() => setSelectedCells(prev => toggleCell(prev, val))}>{children}</td>
+    }
+)
